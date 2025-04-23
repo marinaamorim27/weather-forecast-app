4a0db95801075633a48f98a719b12665e72f51b6
Marina Ribeiro Amorim <marina.amorim@efacec.com>
2025-04-23 16:57:58 +0100

    build pronto para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [cidade, setCidade] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '1rem' }}>
+      <h1>Previsão de 5 Dias</h1>
+      <input
+        type="text"
+        placeholder="Escreve uma cidade"
+        value={cidade}
+        onChange={e => setCidade(e.target.value)}
+      />
+      <button>Pesquisar</button>
     </div>
   );
 }
